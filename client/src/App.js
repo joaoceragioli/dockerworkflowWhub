679b476b2c1818be174cd9653a6a24b24802b72a
joaoceragioli <dodo.jpgta@gmail.com>
2019-11-28 13:04:02 -0300

    title name

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,7 @@ function App() {
     <Router>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
+        <h1 className="App-title">Fib Calculator</h1>
         <Link to="/">Home</Link>
         <Link to="/otherpage">OtherPage</Link>
       </header>
